13044cc4c8d3e4d568326aa86b424789576ca701
emonkarmoker <emonkarmaker101@gmail.com>
2025-10-09 23:36:59 +0600

    break

--- sketch.js
+++ sketch.js
@@ -1,1 +1,20 @@
-alert('Hello World')
+function setup(){
+    createCanvas(800,800);
+
+}
+function draw(){
+    //background(200);
+    //point(200,200);
+    //line(200,200,300,300);
+    //triangle(100, 200, 300, 400, 150, 450);
+    //rect(500, 200, 100);
+    fill(132,100,34);
+    stroke(255,0,0);
+    ellipse(100, 200, 100, 100);
+    strokeWeight(5);
+    stroke(0, 255, 0);
+    ellipse(250, 200, 100, 100);
+    ellipse(400, 200, 100, 100);
+    ellipse(550, 200, 100, 100);
+    ellipse(700, 200, 100, 100);
+}
